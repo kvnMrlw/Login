6c45fdc927c9e7981e3c34785eacd4e54b4636f3
kvnMrlw <kevinmarlow2010@gmail.com>
2026-09-07 06:44:17 -0300

    feat: implementação do formulario de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        return HttpResponse(senha)
+
+    
+def login(request):
+    return render(request, 'login.html')
