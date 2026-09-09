152dcb8a403223c54f438e8da74b915e18fd3fda
kvnMrlw <kevinmarlow2010@gmail.com>
2026-09-09 19:37:17 -0300

    feat: realização da api de login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as log
+from django.contrib.auth.decorators import login_required
 
 def cadastro(request):
     if request.method == "GET":
@@ -9,8 +13,32 @@ def cadastro(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
-        return HttpResponse(senha)
 
-    
+        user = User.objects.filter(username=username).first()
+        if user:
+            return HttpResponse("Esse nome já existe")
+
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        
+
+       
+        return HttpResponse("usuário cadastrado com sucesso!")
+        
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST": 
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            log(request, user)
+            return HttpResponse('Autenticado')
+        else:
+            return HttpResponse('Usuario ou senha incorreto!')
+
+@login_required
+def plataforma(request):
+    return HttpResponse('Erro ao tentar logar')
